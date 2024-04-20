f40a925bb81f47a409bca9fa4d4f1f7964977f16
Alexey <pro100lehsa@gmail.com>
2024-04-20 17:57:57 +0300

    added backend

---
+++ auth.php
@@ -0,0 +1,62 @@
+<?php
+$hideHeader = true;
+include_once './php/header.php';
+?>
+<body>
+    <main id="auth-page" class="auth-page-login">
+        <div class="auth_container">
+            <div class="auth-textblock">
+                <h2>Вход</h2>
+                <p>Для продолжения работы на сайте вам необходимо войти в аккаунт.</p>
+                <span>Нет аккаунта?<a href="vk.com" class="goToReg"> Регистрация.</a></span>
+            </div>
+            <form action="./php/scripts/auth.php" method="post">
+                <input type="email" name="email" id="email" placeholder="Почта">
+                <input type="password" name="password" id="password" placeholder="Пароль">
+                <input type="hidden" name="auth" value="login">
+                <button type="submit">Продолжить</button>
+                <p>Нажимая кнопку "Продолжить", я даю согласие на обработку своих персональных данных в соответствии с Условиями</p>
+            </form>
+        </div>
+    </main>
+    <main id="auth-page" class="auth-page-reg" style="display: none; position: absolute;">
+        <div class="auth_container">
+            <div class="auth-textblock">
+                <h2>Регистрация</h2>
+                <p>Для продолжения работы на сайте вам необходимо зарегистрировать аккаунт.</p>
+                <span>Есть аккаунт?<a href="" class="goToLogin"> Войти.</a></span>
+            </div>
+            <form action="./php/scripts/auth.php" method="post">
+                <input type="text" name="name" id="" placeholder="Имя">
+                <input type="text" name="surname" id="" placeholder="Фамилия">
+                <input type="email" name="email" id="" placeholder="Почта">
+                <input type="email" name="number" id="" placeholder="Телефон">
+                <input type="password" name="password" id="" placeholder="Пароль">
+                <input type="password" name="password" id="" placeholder="Подтверждение">
+                <input type="hidden" name="auth" value="reg">
+                <button type="submit">Продолжить</button>
+                <p>Нажимая кнопку "Продолжить", я даю согласие на обработку своих персональных данных в соответствии с Условиями</p>
+            </form>
+        </div>
+    </main>
+</body>
+
+<script>
+    $('.goToLogin').click(function(e) {
+    e.preventDefault();
+    $('.auth-page-reg').css('display', 'none'); 
+    $('.auth-page-reg').css('position', 'absolute');
+    
+    $('.auth-page-login').css('display', 'flex'); 
+    $('.auth-page-login').css('position', 'static'); 
+})
+
+$('.goToReg').click(function(e) {
+    e.preventDefault();
+    $('.auth-page-login').css('display', 'none'); 
+    $('.auth-page-login').css('position', 'absolute');
+    
+    $('.auth-page-reg').css('display', 'flex'); 
+    $('.auth-page-reg').css('position', 'static'); 
+})
+</script>
---
+++ php/scripts/auth.php
@@ -0,0 +1,12 @@
+<?=
+
+$type = $_POST["auth"];
+$type = $_POST["name"];
+$type = $_POST["surname"];
+$type = $_POST["email"];
+$type = $_POST["number"];
+$type = $_POST["password"];
+$type = $_POST["password_repeat"];
+
+
+
